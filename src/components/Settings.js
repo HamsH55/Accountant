46117ec19f8eca38a03aefce14b0eb432534690c
HamsH55 <hams@ditech.sa>
2024-04-07 13:58:05 +0300

    push1

---
+++ src/components/Settings.js
@@ -0,0 +1,21 @@
+export default function Settings(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+
+// import React from 'react';
+
+// const Settings = () => {
+//     return (
+//         <div>
+            
+//         </div>
+//     );
+// };
+
+// export default Settings;
