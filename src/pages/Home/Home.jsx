ca1a9f9b08b6bfba2791f81b2d474849022f66f3
mehedi1802hasan <mehedi1802hasan@gmail.com>
2024-04-08 19:58:17 +0600

    testing commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div>
-            <h3 className='text-pink-500 font-bold text-xl'>HomeSection--Comming Soon...</h3>
+            <h3 className='text-pink-500 bg-green-400 font-bold text-xl'>HomeSection--Comming soon.. .</h3>
             <button className='w-36 h-10 text-center border border-yellow-500 rounded-lg hover:bg-yellow-500 hover:font-bold'><Link to='/dashboard'>Go to Dashboard</Link></button>
             <button className='w-36 h-10 text-center border border-yellow-500 rounded-lg hover:bg-yellow-500 hover:font-bold'><Link to='/login'>Go to LoginPage</Link></button>
         </div>
